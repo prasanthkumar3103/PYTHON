c250d98596d0cda153a631d589697ee259ba305f
prasanthkpamula@gmail.com <prasanthbangs@gmail.com>
2018-02-27 08:18:26 +0530

    Day-1

---
+++ script01.py
@@ -0,0 +1,3 @@
+	print ("my first name is: prashanth)
+	print ("my middle name is: kumar)
+	print ("my last name is: pamula)
---
+++ script02.py
@@ -0,0 +1,6 @@
+	print ("my first name is: prashanth)
+	print ("my middle name is: kumar)
+	print ("my last name is: pamula)
+	waitforresponce = input("Press any Key to terminate")
+	
+	#waitforresponce  -- is a variable for input device in windows environment
